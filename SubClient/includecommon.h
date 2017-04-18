0b16be72f506d3f2950e8e4d47dce5cf904eb07f
ws <2422523675@qq.com>
2017-04-18 21:57:55 +0800

    添加tinyxml支持

---
+++ SubClient/includecommon.h
@@ -0,0 +1,8 @@
+#ifndef PROGRAMONCE_HEADER
+#define PROGRAMONCE_HEADER
+
+//引入第三方模块：tinyxml
+#include "../ThirdPackage/tinystr.h"
+#include "../ThirdPackage/tinyxml.h"
+
+#endif
---
+++ SubClient/modelandmcontrol.cpp
@@ -0,0 +1,14 @@
+#include "includecommon.h"
+
+
+/*模型与模型控制器
+**程序启动配置参数
+**Processor.exe -fpath xmlfilepath
+**程序名		指明加载的源文件路径
+**Processor.exe -help
+**显示程序帮助信息
+**/
+
+int main(int argc, char* argv[]) {
+
+}
