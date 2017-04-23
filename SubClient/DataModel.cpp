1f415684fde29e9c2f060b20d016fe8aad22b861
ws <2422523675@qq.com>
2017-04-23 12:24:21 +0800

    增加DotaModel对外接口
    实现了InitModel接口，装载数据模型文件。
    实现了LocateModule接口，定位模型节点。
    验证了FileOperate的Exist函数实现没有问题。

--- SubClient/DataModel.cpp
+++ SubClient/DataModel.cpp
@@ -3,18 +3,67 @@
 
 
 DataModel::DataModel()
-	: doc(NULL)
 {
+	doc = new TiXmlDocument();
+	RootElm = nullptr;
 }
 
 
 DataModel::~DataModel()
 {
+	delete this->RootElm;
+	delete this->doc;
 }
 
 
 // 根据确切的文件路径载入模型数据文件
 int DataModel::InitModel(char* procFilePath)
+{
+	this->doc->LoadFile(procFilePath);
+	this->RootElm = this->doc->RootElement();
+	return 0;
+}
+
+
+// 定位一个模块节点
+int DataModel::LocateModule(char* m_id, TiXmlElement** moduleElmRef)
+{
+	TiXmlElement* moduleC = this->RootElm->FirstChildElement("modulecollect");
+	TiXmlElement* moduleOne = moduleC->FirstChildElement();
+
+	while (strcmp(moduleOne->Attribute("m_id"), m_id)) {
+		moduleOne = moduleOne->NextSiblingElement();
+	}
+
+	*moduleElmRef = moduleOne;
+
+	return 0;
+}
+
+
+// 定位一个模块的branch节点
+int DataModel::LocateBranch(TiXmlElement* moduleElm, char* b_id, TiXmlElement** branchElmRef)
+{
+	return 0;
+}
+
+
+// 定位属于模块的变量节点
+int DataModel::LocateVariable(TiXmlElement* moduleElm, char* v_id, TiXmlElement** varElmRef)
+{
+	return 0;
+}
+
+
+// 精确获取一个relate节点，relate节点是一个次级节点，因此利用R_id可以唯一确定
+int DataModel::GetRelate(char* r_id)
+{
+	return 0;
+}
+
+
+// 根据指定的分支id，获取属于该分支的下一个节点，有状态方法
+int DataModel::GetNextOneOfRelates(char* module_branch_id)
 {
 	return 0;
 }
--- SubClient/DataModel.h
+++ SubClient/DataModel.h
@@ -15,6 +15,25 @@ public:
 	virtual int InitModel(char* procFilePath);
 private:
 	TiXmlDocument* doc;
+public:
+	// 定位一个模块节点
+	// 模块节点是一个次级节点，可以由一个id直接确定
+	virtual int LocateModule(char* m_id, TiXmlElement** moduleElmRef);
+	// 定位一个模块的branch节点
+	// branch节点是一个三级节点需要由module_id与branch_id共同确定
+	virtual int LocateBranch(TiXmlElement* moduleElm, char* b_id, TiXmlElement** branchElmRef);
+	// 定位属于模块的变量节点
+	// variable节点是一个三级节点，需要由module_id与variable_id共同确定
+	virtual int LocateVariable(TiXmlElement* moduleElm, char* v_id, TiXmlElement** varElmRef);
+private:
+	TiXmlElement* RootElm;
+public:
+	// 精确获取一个relate节点，relate节点是一个次级节点，因此利用R_id可以唯一确定
+	virtual int GetRelate(char* r_id);
+	// 根据指定的分支id，获取属于该分支的下一个节点，有状态方法
+	// 如果第一次调用该方法或者id与上次调用不同，模型从头遍历节点树，
+	// 如果两次调用相同，模型紧跟着上次结果取下一个节点。
+	virtual int GetNextOneOfRelates(char* module_branch_id);
 };
 
 #endif
