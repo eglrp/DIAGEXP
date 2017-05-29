e16b35bd24796b85382cf1d2c0b19fe576029768
ws <2422523675@qq.com>
2017-05-29 23:15:32 +0800

    部分窗口资源绑定
    动态变化的分支汇总窗口采用了数据绑定的策略，当从核心模块提取信息之后，界面窗口将自动更新列表信息。

--- DIAGEXP/MainWindow.xaml.cs
+++ DIAGEXP/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DIAGEXP
 {
@@ -20,29 +10,104 @@ namespace DIAGEXP
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<BranchRecord> visiableRecordC = new ObservableCollection<BranchRecord>();
+        private ObservableCollection<BranchRecord> allRecordC = new ObservableCollection<BranchRecord>();
+
         public MainWindow()
         {
             InitializeComponent();
             ScrollOperation.ScrollToHorizontalOffset(BaseP.Width/5);
             ScrollOperation.ScrollToVerticalOffset(BaseP.Height/5);
+            NewBranchTypeList.ItemsSource = getBranchTypeCollection();
+            VisiableBranchCollection.ItemsSource = visiableRecordC;
+            AllBranchCollection.ItemsSource = allRecordC;
+        }
+        
+        private void GiveOutAAboutWindow(object sender, RoutedEventArgs e)
+        {
+            Window about = new AboutWindows();
+            about.ShowDialog();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //该集合不需要更新
+        private List<BranchRecord> getBranchTypeCollection()
         {
-            BaseP.Width = BaseP.Width * 10;
-            BaseP.Height = BaseP.Height*10;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            BaseP.Width = BaseP.Width / 10;
-            BaseP.Height = BaseP.Height / 10;
+            List<BranchRecord> some = new List<BranchRecord>();
+            some.Add(new BranchRecord("alsklsdjfa.asldfkajsdlkf","this.One", "yes!!", "注释信息for ONE", "rectangle"));
+            some.Add(new BranchRecord("alskdfasdf.slkdjflaksjdf","this.Two", "yes!!", "注释信息for TWO", "rectangle"));
+            some.Add(new BranchRecord("alskdfasdf.sdlkfalskdjfk","this.Three", "no!!", "注释信息for THREE", "rectangle"));
+            some.Add(new BranchRecord("alskdfalsd.asldkfjalkdjf","this.Four", "no!!", "注释信息for FOUR", "rectangle"));
+            some.Add(new BranchRecord("alskdfasdf.asdlfkasldkff","this.Five", "no!!", "注释信息for FIVE", "rectangle"));
+            return some;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Window about = new AboutWindows();
-            about.ShowDialog();
+            visiableRecordC.Add(new BranchRecord("aldjfasf", "asldkfjalskdjf", "no!!", "zhushixinxi", "rectangle"));
+            visiableRecordC.Add(new BranchRecord("aldjfasf", "asldkfjalskdjf", "no!!", "zhushixinxi", "rectangle"));
+            allRecordC.Add(new BranchRecord("aldjfasf", "asldkfjalskdjf", "no!!", "zhushixinxi", "rectangle"));
+            allRecordC.Add(new BranchRecord("aldjfasf", "asldkfjalskdjf", "no!!", "zhushixinxi", "rectangle"));
+        }
+    }
+
+
+    // 记录代表条目
+    public class BranchRecord:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        private string itemID;
+        public string ItemID {
+            get { return itemID; }
+            set { itemID = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("BranchRecord"));
+            }
+        }
+        private string branchName;
+        public string BranchName
+        {
+            get { return branchName; }
+            set { branchName = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("BranchRecord"));
+            }
+        }
+        private string staticOrNot;
+        public string StaticV
+        {
+            get { return staticOrNot; }
+            set { staticOrNot = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("BranchRecord"));
+            }
+        }
+        private string animation;
+        public string Animation
+        {
+            get { return animation; }
+            set { animation = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("BranchRecord"));
+            }
+        }
+        private string shapeType;
+
+        public string ShapeType
+        {
+            get { return shapeType; }
+            set { shapeType = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("BranchRecord"));
+            }
+        }
+        public BranchRecord(string itemID,string branchName,string staticW,string animation, string shapeType)
+        {
+            ItemID = itemID;
+            StaticV = staticW;
+            BranchName = branchName;
+            Animation = animation;
+            ShapeType = shapeType;
         }
     }
 }
